eae1ab824869d368c336679889cd782de43d9a7b
unknown <rafaelbenetti@SL-002591.cwinet.local>
2015-09-24 19:23:43 -0300

    add chapter2.2

---
+++ source/chapter2.3/notesApp.js
@@ -0,0 +1,20 @@
+angular.module('notesApp', [])
+       .controller('MainController', [function(){
+         var self = this;
+         self.notes =
+          [{
+             id: 1,
+             label: 'First Note',
+             done: false
+           },
+           {
+             id: 2,
+             label: 'Second Note',
+             done: false
+           },
+           {
+             id: 3,
+             label: 'Finished Third Note',
+             done: true
+           }];
+       }]);
