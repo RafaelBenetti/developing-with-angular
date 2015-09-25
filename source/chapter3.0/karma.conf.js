1b3daebd3ca3f9eb1f52da84af22ed80eb05a4fb
unknown <rafaelbenetti@SL-002591.cwinet.local>
2015-09-25 10:28:29 -0300

    file to config karma

---
+++ source/chapter3.0/karma.conf.js
@@ -0,0 +1,38 @@
+module.export = function(config) {
+  config.set({
+    // path de base que será usado para resolver os nomes dos arquivos e exclusões.
+    pathBase = ''.
+    // framework de testes a ser usado (jasmine/mocha/qunit/...)
+    frameworks = ['jasmine'],
+    // lista de arquivos/padrões a serem carregados no navegador.
+    files = [
+      'angular.min.js',
+      'angular-mocks.js',
+      'contrller.js',
+      'simpleSpec.js',
+      'controllerSpec.js'
+    ],
+    // lista de arquivos/padrões a serem excluídos.
+    exclude = [],
+    // porta do servidor web.
+    port: 8080,
+    // nível de logging.
+    // valores possíveis: LOG_DISABLE \\ LOG_ERROR \\ LOG_WARN \\ LOG_INFO \\ LOG_DEBUG.
+    logLevel: config.LOG_INFO,
+    // habilita/desabilita a observação de arquivos e a execução dos testes.
+    // sempre que algum arquivo for alterado.
+    autoWatch: true,
+    // inicia eses navegadores, atualmente disponíveis:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (somente no mac)
+    // - PhantomJS
+    // - IE (somente no Windows)
+    browsers: ['Chrome'],
+    // modo de integração contínua.
+    // se for true, captura os navegadores, executa os testes e sai.
+    singleRun: false
+  })
+};
