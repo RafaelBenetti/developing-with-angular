cfc32eed455347a710851646c4bbe98065dce49a
Rafael Benetti <rafaeldeoliveirabenetti@gmail.com>
2015-09-26 09:25:31 -0300

    add two forms in one page

--- source/chapter4.3/notesApp.js
+++ source/chapter4.3/notesApp.js
@@ -1,7 +1,12 @@
 angular.module('notesApp', [])
        .controller('MainController', [function(){
          var self = this;
-         self.submit = function(){
-           console.log('User clicked submit with', self.user);
+         self.submit1 = function(){
+           // cria o objeto para enviar ao servidor.
+           var user = { username: self.userName, password: self.password };
+           console.log('First form submit with ', user);
+         };
+         self.submit2 = function(){
+           console.log('Second form submit with ', self.user);
          };
        }]);
