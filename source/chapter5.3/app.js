20a42f9a49ae24075c7fc659ad61ca06b00dd693
Rafael Benetti <rafaeldeoliveirabenetti@gmail.com>
2015-09-27 21:20:44 +0800

    add a service example

---
+++ source/chapter5.3/app.js
@@ -0,0 +1,36 @@
+function ItemService () {
+    var self = this;
+    var items = [
+      { id: 1, label: 'Item 0' },
+      { id: 2, label: 'Item 1' }
+    ];
+
+    self.list = function(){
+      return items;
+    };
+    self.add = function(item){
+      items.push(item);
+    };
+}
+
+angular.module('notesApp', [])
+  .service('ItemService', [ItemService])
+  .controller('MainCtrl', [function(){
+    var self = this;
+    self.tab = 'first';
+    self.open = function(){
+      self.tab = tab;
+    };
+  }])
+  .controller('SubCtrl', [function(ItemService){
+    var self = this;
+    self.this = function(){
+      return ItemService.list();
+    };
+    self.add = function(){
+      ItemService.add({
+        id: self.list().length + 1,
+        label: 'Item ' + self.list().length
+      });
+    };
+  }]);
